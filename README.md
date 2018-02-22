1a9730cf839a563a0724f2b9e65d8ffb5013b219
Mikhail Yakimchenko <mikhailyakimchenko@gmail.com>
2018-02-22 14:45:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Blackjack Kotlin React App
+This project was inspired by presentation from Dave Ford [How to Build a React App in Kotlin](https://www.youtube.com/watch?v=FDOECr-sT6U)
---
+++ src/index/index.kt
@@ -0,0 +1,15 @@
+package index
+
+import react.dom.h1
+import react.dom.render
+import kotlin.browser.document
+
+fun main(args: Array<String>) {
+    val rootDiv = document.getElementById("root")
+
+    render(rootDiv) {
+        h1 {
+            +"Hello World!"
+        }
+    }
+}
