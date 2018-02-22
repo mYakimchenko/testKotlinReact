c4ce7a0b8b9aa4bdcec1a47ba0a500e249d5f95c
Mikhail Yakimchenko <mikhailyakimchenko@gmail.com>
2018-02-22 15:05:06 +0300

    Working with kotlin html extensions

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,15 +1,35 @@
 package index
 
-import react.dom.h1
-import react.dom.render
+import kotlinx.html.title
+import react.dom.*
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
     val rootDiv = document.getElementById("root")
 
     render(rootDiv) {
-        h1 {
-            +"Hello World!"
+        div {
+            h1 { +"Blackjack" }
+            div {
+                attrs {
+                    title = "Foo"
+                    jsStyle {
+                        display = "Flex"
+                    }
+                }
+
+                div {
+                    div { b { +"Player Hand" } }
+                    div { +"cards goes here" }
+                    div { b { +"12 Points" } }
+                }
+
+                div {
+                    div { b { +"Dealer Hand" } }
+                    div { +"cards goes here" }
+                    div { b { +"14 Points" } }
+                }
+            }
         }
     }
 }
